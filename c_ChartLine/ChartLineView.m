9724dea9e5fe2d156781541a93ac4d1a97ccc9bc
cailei <cailei@caileideMacBook-Pro.local>
2015-07-13 17:47:57 +0800

    画线，颜色

--- c_ChartLine/ChartLineView.m
+++ c_ChartLine/ChartLineView.m
@@ -59,7 +59,7 @@
         CGPathMoveToPoint(path, NULL, xStart, PADDING+(i*availableHeight)/(x-1));
         CGPathAddLineToPoint(path, NULL,xStart+availableWidth, PADDING+(i*availableHeight)/(x-1));
         CGContextAddPath(c, path);
-        CGContextSetStrokeColorWithColor(c, [[UIColor darkGrayColor] CGColor]);//设置颜色
+        CGContextSetStrokeColorWithColor(c, [[UIColor colorWithWhite:240/255.0 alpha:1] CGColor]);//设置颜色
         CGContextStrokePath(c);
         CGPathRelease(path);
     }
@@ -69,7 +69,7 @@
         CGPathMoveToPoint(path, NULL, xStart+(i*availableWidth)/(y-1), PADDING);
         CGPathAddLineToPoint(path, NULL, xStart+(i*availableWidth)/(y-1), PADDING+availableHeight);
         CGContextAddPath(c, path);
-        CGContextSetStrokeColorWithColor(c, [[UIColor darkGrayColor] CGColor]);//设置颜色
+        CGContextSetStrokeColorWithColor(c, [[UIColor colorWithWhite:240/255.0 alpha:1] CGColor]);//设置颜色
         CGContextStrokePath(c);
         CGPathRelease(path);
     }
@@ -86,31 +86,49 @@
     //第三部 画点
     for (int i=0; i<[dataArray count]; i++) {
         Float32 count_num = [dataArray[i][@"value"] floatValue];
-        CGContextSetFillColorWithColor(c, [[UIColor redColor] CGColor]);
-        CGContextFillEllipseInRect(c, CGRectMake(xStart+(i*availableWidth)/(y-1)-3, (1-(count_num/(up)))*availableHeight+PADDING-3, 5, 5));
+        CGContextSetFillColorWithColor(c, [UIColor colorWithRed:255/255.0 green:159/255.0 blue:56/255.0 alpha:1].CGColor);
+        CGContextFillEllipseInRect(c, CGRectMake(xStart+(i*availableWidth)/(y-1)-6, (1-(count_num/(up)))*availableHeight+PADDING-6, 10, 10));
     }
     
-    //第四部 把点连成线
-    
-    
-    for (int i=0; i<y-1; i++) {
+    //第四部 设置底部区域的颜色
+    {
         CGMutablePathRef path = CGPathCreateMutable();//句柄
-        CGPathMoveToPoint(path, NULL, xStart+(i*availableWidth)/(y-1), (1-([dataArray[i][@"value"] floatValue]/(up)))*availableHeight+PADDING);
-        CGPathAddLineToPoint(path, NULL, xStart+((i+1)*availableWidth)/(y-1), (1-([dataArray[i+1][@"value"] floatValue]/(up)))*availableHeight+PADDING);
-        CGContextAddPath(c, path);
-        CGContextSetStrokeColorWithColor(c, [[UIColor darkGrayColor] CGColor]);//设置颜色
-        CGContextStrokePath(c);
+//        CGPathMoveToPoint(path, NULL, xStart+(0*availableWidth)/(y-1), (1-([dataArray[0][@"value"] floatValue]/(up)))*availableHeight+PADDING);
+        CGContextMoveToPoint(c, xStart+(0*availableWidth)/(y-1), (1-([dataArray[0][@"value"] floatValue]/(up)))*availableHeight+PADDING);
+        for (int i=0; i<y-1; i++) {
+             CGContextAddLineToPoint(c, xStart+((i+1)*availableWidth)/(y-1), (1-([dataArray[i+1][@"value"] floatValue]/(up)))* availableHeight+PADDING);
+            
+//            CGContextAddPath(c, path);
+//            CGContextSetStrokeColorWithColor(c, [[UIColor clearColor] CGColor]);//设置颜色
+//            CGContextStrokePath(c);
+            
+        }
+        
+        CGContextAddLineToPoint(c,xStart+((y-1)*availableWidth)/(y-1), PADDING+availableHeight);
+        CGContextAddLineToPoint(c, xStart+(0*availableWidth)/(y-1), PADDING+availableHeight);
+        
+        CGContextClosePath(c);
+        CGContextSetFillColorWithColor(c, [UIColor colorWithRed:255/255.0 green:159/255.0 blue:56/255.0 alpha:0.1].CGColor);
+        CGContextFillPath(c);
         CGPathRelease(path);
+    
     }
-    
-    
-//    CGMutablePathRef path = CGPathCreateMutable();//句柄
-//    
-//    
-//    
-//    //移动到第一个点的位置
-
+    //第五部画线
+    {
+        
+        for (int i=0; i<y-1; i++) {
+            CGMutablePathRef path = CGPathCreateMutable();//句柄
+            CGPathMoveToPoint(path, NULL, xStart+(i*availableWidth)/(y-1), (1-([dataArray[i][@"value"] floatValue]/(up)))*availableHeight+PADDING);
+            CGPathAddLineToPoint(path, NULL, xStart+((i+1)*availableWidth)/(y-1), (1-([dataArray[i+1][@"value"] floatValue]/(up)))*availableHeight+PADDING);
+            CGContextAddPath(c, path);
+            CGContextSetStrokeColorWithColor(c, [UIColor colorWithRed:255/255.0 green:159/255.0 blue:56/255.0 alpha:1].CGColor);//设置颜色
+            CGContextStrokePath(c);
+            CGPathRelease(path);
+        }
+    }
 }
+
+//接下来增加点击事件
 
 
 @end
